ad4c80155f531b23a73cf5d9152a5e2533010d9c
Nicola Aitken <nicola@geckoboard.com>
2016-03-12 15:53:11 +0000

    CRUD for assignments model

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -7,9 +7,29 @@ class AssignmentsController < ApplicationController
   def new
   end
 
+  def edit
+    @assignment = Assignment.find(params[:id])
+  end
+
   def create
     @assignment = Assignment.new(assignment_params)
     @assignment.save
+
+    redirect_to action: "index"
+  end
+
+  def update
+    @assignment = Assignment.find(params[:id])
+    if @assignment.update(article_params)
+      redirect_to action: "index"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @assignment = Assignment.find(params[:id])
+    @assignment.destroy
 
     redirect_to action: "index"
   end
